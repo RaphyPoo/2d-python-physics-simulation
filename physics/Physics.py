c0019079526150f99da96d9244a673f64c6e35b6
Raphael Canciani <raphael.canciani@ariv.ca>
2023-09-22 16:04:28 -0400

    Fixed the import error and the rendering.

--- physics/Physics.py
+++ physics/Physics.py
@@ -1,24 +1,21 @@
 import pymunk
 
 
-def create_circle(space, pos):
-    body = pymunk.Body(1, 100, body_type=pymunk.Body.DYNAMIC)
-    body.position = pos
-    shape = pymunk.Circle(body, 80)
-    space.add(body, shape)
-    return shape
-
-
-def create_static_circle(space, pos):
-    body = pymunk.Body(body_type=pymunk.Body.STATIC)
-    body.position = pos
-    shape = pymunk.Circle(body, 50)
-    space.add(body, shape)
-    return shape
-
-
 class Physics:
     def __init__(self):
         self.space = pymunk.Space()
-        self.space.step(1 / 50)
         self.space.gravity = (0, 20)
+
+    def create_circle(self, pos):
+        body = pymunk.Body(1, 100, body_type=pymunk.Body.DYNAMIC)
+        body.position = pos
+        shape = pymunk.Circle(body, 50)
+        self.space.add(body, shape)
+        return shape
+
+    def create_static_circle(self, pos):
+        body = pymunk.Body(body_type=pymunk.Body.STATIC)
+        body.position = pos
+        shape = pymunk.Circle(body, 50)
+        space.add(body, shape)
+        return shape
--- rendering/Render.py
+++ rendering/Render.py
@@ -5,34 +5,35 @@ from physics.Physics import Physics
 
 class Render:
     def __init__(self):
+        pygame.init()
         self.screen = pygame.display.set_mode((800, 800))  # creating the display surface
-        self.screen.fill((0, 0, 0))
         self.clock = pygame.time.Clock()
         self.Physics = Physics()
-        pygame.init()
 
     def run(self):
+        circles = []
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
-                # if event.type == pygame.MOUSEBUTTONDOWN:
-                # create_apple(space, event.pos)
-                # apples.append(create_apple(space, event.pos))
-        self.screen.fill((0, 0, 0))
-        space.step(1 / 50)
-        pygame.display.update()
-        clock.tick(120)
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    circles.append(self.Physics.create_circle(event.pos))
+
+            self.screen.fill((0, 0, 0))
+            self.Physics.space.step(1 / 50)
+            self.draw_circles(circles)
+            pygame.display.update()
+            self.clock.tick(120)
 
     def draw_static_ball(self, balls):
         for ball in balls:
             pos_x = int(ball.body.position.x)
             pos_y = int(ball.body.position.y)
-            pygame.draw.circle(self.screen, (255, 255, 255), (pos_x, pos_y), 50)
+            pygame.draw.circle(self.screen, (255, 255, 255), (pos_x, pos_y), balls.radius)
 
-    def draw_apples(self, apples):
-        for apple in apples:
-            pos_x = int(apple.body.position.x)
-            pos_y = int(apple.body.position.y)
-            pygame.draw.circle(self.screen, (255, 255, 255), (pos_x, pos_y), 80)
+    def draw_circles(self, circles):
+        for circle in circles:
+            pos_x = int(circle.body.position.x)
+            pos_y = int(circle.body.position.y)
+            pygame.draw.circle(self.screen, (255, 255, 255), (pos_x, pos_y), circle.radius)
